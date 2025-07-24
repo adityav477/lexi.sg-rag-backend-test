d4e0dd1c79c34e54c25c8b58908b26b3bd5ec300
aditya <adityavish126@gmail.com>
2025-07-24 23:25:46 +0530

    due to low memory in render

--- perplexity_llm.py
+++ perplexity_llm.py
@@ -2,7 +2,6 @@ import getpass
 import os
 from langchain_perplexity import ChatPerplexity
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_ollama.llms import OllamaLLM
 from dotenv import load_dotenv
 from pydantic import SecretStr
 from embeddings.vector_store import vector_store
@@ -31,8 +30,6 @@ def get_answer(query: str):
         timeout=0,
         streaming=True,
     )
-
-    local_chat = OllamaLLM(model="steamdj/llama3.1-cpu-only", temperature=0)
 
     template = """You are a legal assistant trained in Indian law. Your job is to analyze retrieved legal text passages and generate a precise, well-grounded answer to a user's query, including citations to the relevant legal source documents.
 
@@ -66,7 +63,6 @@ def get_answer(query: str):
     prompt = ChatPromptTemplate.from_messages([("system", template), ("human", query)])
 
     llm = prompt | chat
-    local_llm = prompt | local_chat
 
     response = llm.invoke({"query": query, "context_chunks": context_chunks})
 
